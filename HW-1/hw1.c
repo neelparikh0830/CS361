7c4195bc190fdc2d1507b9576018b5aa8128b4f2
Neel Parikh <120353285+neelparikh0830@users.noreply.github.com>
2023-01-18 19:28:58 -0600

    Add files via upload

---
+++ HW-1/hw1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int but_the;
+static int order = 5;
+static int isnt = 0;
+double types = 5;
+const int scopes = 50;
+char* and_the = "Nike";
+int *const indices = &isnt;
+extern int definitely;
+
+int static important(){
+	return types;
+}
+
+int matter(){
+	int static for_these = 6;
+	int static symbols = 0;
+	isnt = isnt + order;
+	puts("npari3");
+	float neel  = symbols + for_these;
+	return neel + definitely;
+}
+
+int main(int argc,char* argv[]){
+    important();
+    return 0;
+}
